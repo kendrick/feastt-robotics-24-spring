2084fa534e1acb4d3a400c4ea73db02fd27b7c43
Kendrick Arnett <kmarnett@gmail.com>
2024-02-25 14:45:57 -0600

    Return boolean for RTC detection.

--- src/Helpers.h
+++ src/Helpers.h
@@ -5,10 +5,11 @@
 #include <RTClib.h>
 #include <Music.h>
 
-void initRTC(RTC_DS3231 rtc) {
+bool initRTC(RTC_DS3231 rtc) {
   if (!rtc.begin())
   {
     Serial.println("Couldn't find RTC.");
+    return false;
   }
 
   // Check if the RTC lost power and if so, set the time to the time this sketch was compiled
@@ -18,6 +19,8 @@ void initRTC(RTC_DS3231 rtc) {
     // The following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+
+  return true;
 }
 
 #endif
